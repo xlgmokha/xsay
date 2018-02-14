a4eee38a7cd12e74650ef308d8412cd695315546
mokha <mokha@cisco.com>
2018-02-13 19:12:06 -0700

    add cat say.

--- lib/xsay.rb
+++ lib/xsay.rb
@@ -1,5 +1,27 @@
 require "xsay/version"
+require "thor"
 
 module Xsay
-  # Your code goes here...
+  class CLI < Thor
+    desc "cat <message>", "xsay cat meow"
+    def cat(message)
+      line_break = "-" * message.length
+      say <<-MESSAGE
+  #{line_break}
+< #{message} >
+  #{line_break}
+   \\ \\
+    ("`-''-/").___..--''"`-._
+     `6_ 6  )   `-.  (     ).`-.__.`)
+     (_Y_.)'  ._   )  `._ `. ``-..-'
+   _..`--'_..-_/  /--'_.' ,'
+  (il),-''  (li),'  ((!.-'
+      MESSAGE
+    end
+
+    private
+
+    def render(message, template)
+    end
+  end
 end
--- spec/xsay_spec.rb
+++ spec/xsay_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Xsay do
   it "has a version number" do
     expect(Xsay::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
